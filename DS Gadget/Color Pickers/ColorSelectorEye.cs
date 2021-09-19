381f3ef9396a46f79fd221bab30fa7094426bc66
Norgaren <83100346+Nordgaren@users.noreply.github.com>
2021-09-19 13:05:32 -0700

    comments and removed window controls from color picker

--- DS Gadget/Color Pickers/ColorSelectorEye.cs
+++ DS Gadget/Color Pickers/ColorSelectorEye.cs
@@ -27,8 +27,8 @@ namespace DS_Gadget
             G = Hook.EyeColorGreen;
             B = Hook.EyeColorBlue;
 
+            //Returns wether or not the hair was glowy or not and sets the RGB values accordingly
             var glow = SetGlowStatus();
-
             nudRed.Value = glow ? (byte)(((R / 10) * 255)) : (byte)(R * 255);
             nudGreen.Value = glow ? (byte)(((G / 10) * 255)) : (byte)(G * 255);
             nudBlue.Value = glow ? (byte)(((B / 10) * 255)) : (byte)(B * 255);
@@ -42,13 +42,14 @@ namespace DS_Gadget
             label.Text = gbxColorSelector.Text;
             gbxColorSelector.Text = "";
             label.Left = gbxColorSelector.Left + (gbxColorSelector.Width - label.Width) / 2;
-            label.Top = gbxColorSelector.Top + 2; // 2 is an example : adjust the constant
+            label.Top = gbxColorSelector.Top + 2;
             label.Parent = gbxColorSelector.Parent;
             label.BringToFront();
         }
 
         private bool SetGlowStatus()
         {
+            //If any of the eye color values are over 1, set the glow checkbox to checked
             if (Hook.EyeColorRed > 1 || Hook.EyeColorGreen > 1 || Hook.EyeColorBlue > 1)
                 return cbxGlow.Checked = true;
             else
@@ -57,6 +58,7 @@ namespace DS_Gadget
 
         private void pbxColorSelector_MouseMove(object sender, MouseEventArgs e)
         {
+            //Get the color data of the pixel under the mouse.
             try
             {
                 var clr = PixelData.GetPixel(e.X, e.Y);
@@ -68,12 +70,11 @@ namespace DS_Gadget
                     SetEyeColor(clr);
                 }
             }
-            catch (ArgumentOutOfRangeException)
-            {
-                return;
-            }
+            catch (ArgumentOutOfRangeException) { return; } //Prevents Gadget from crashing if you go outside of the color selector with mouse button down.
         }
 
+
+        //Set selected color panel BG color and RGB value of the nuds to the color passed to the method.
         private void SetEyeColor(Color clr)
         {
             pnlSelectedScreen.BackColor = clr;
@@ -82,6 +83,7 @@ namespace DS_Gadget
             nudBlue.Value = clr.B;
         }
 
+        //Turn the nud values into the hex avlues seen in txtHexColor
         private void UpdateTextBox()
         {
             var red = ((byte)nudRed.Value).ToString("X2");
@@ -97,6 +99,7 @@ namespace DS_Gadget
             UpdateTextBox();
         }
 
+        //Recalculate all colors based on the checkbox status
         private void RecalulateColors()
         {
             Hook.EyeColorRed = cbxGlow.Checked ? (float)((nudRed.Value / 255) * 10) : (float)(nudRed.Value / 255);
@@ -104,6 +107,7 @@ namespace DS_Gadget
             Hook.EyeColorBlue = cbxGlow.Checked ? (float)((nudBlue.Value / 255) * 10) : (float)(nudBlue.Value / 255);
         }
 
+        //Set the eye color back to the color the eyes were when this form opened
         private void btnCancel_Click(object sender, EventArgs e)
         {
             Hook.EyeColorRed = R;
@@ -112,32 +116,35 @@ namespace DS_Gadget
             Close();
         }
 
+        //Close the form
         private void btnApply_Click(object sender, EventArgs e)
         {
             Close();
         }
 
+        //Convert the hex number typed into txtHexColor
         private void txtHexColor_TextChanged(object sender, EventArgs e)
         {
-            var color = txtHexColor.Text.PadRight(6, '0');
+            var color = txtHexColor.Text.PadRight(6, '0'); //Pad the hex number
 
             var red = byte.Parse(color.Substring(0, 2), System.Globalization.NumberStyles.HexNumber);
             var green = byte.Parse(color.Substring(2, 2), System.Globalization.NumberStyles.HexNumber);
             var blue = byte.Parse(color.Substring(4, 2), System.Globalization.NumberStyles.HexNumber);
 
-            if (ActiveControl == sender)
+            if (ActiveControl == sender) //Only recalculate if the event sender is the active control (txtHexColor)
             {
                 var clr = Color.FromArgb(red, green, blue);
                 SetEyeColor(clr);
             }
-
         }
 
+        //Might be unecessary
         private void nud_Leave(object sender, EventArgs e)
         {
             UpdateTextBox();
         }
 
+        //Recalculate colors if the checkbox changed.
         private void cbxGlow_CheckedChanged(object sender, EventArgs e)
         {
             RecalulateColors();
